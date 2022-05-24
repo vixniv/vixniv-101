d89ad525be491620647bfc02c04ad7f3d669707f
vixniv <vixniv@gmail.com>
2022-05-25 00:48:09 +0700

    init and add header

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: "#C0C0C0",
+      secondary: "#F0F0F2",
+    },
+    extend: {
+      fontFamily: {
+        title: ["Space Mono", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+};
